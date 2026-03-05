b73de2986b6fee2f6023b4bf4773b98c2d52f273
Guram Mamporia <gurammamphoria@gmail.com>
2026-03-06 01:10:49 +0400

    added swagger ui to settings and accounts

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,18 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/", include("exercises.urls")),
     path("api/", include("workouts.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
