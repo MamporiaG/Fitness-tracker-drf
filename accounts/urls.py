9a5b50383e06a574d3e7710c4b77c7b955f9cdd3
Guram Mamporia <gurammamphoria@gmail.com>
2026-03-11 00:13:27 +0400

    fixed a bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
