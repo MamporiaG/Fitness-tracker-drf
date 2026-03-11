dc2e5b2e20d77a4ad1168c4a821840ed7b6a96db
Guram Mamporia <gurammamphoria@gmail.com>
2026-03-12 00:31:02 +0400

    revised views.py

--- exercises/views.py
+++ exercises/views.py
@@ -1,53 +1,18 @@
 # exercises/views.py
 
 from rest_framework import generics
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
 from .models import Exercise
 from .serializers import ExerciseSerializer
 
-
-class IsOwnerAdminOrReadOnly(BasePermission):
-    """
-    - All authenticated users can read (GET) and create (POST).
-    - Only the owner or an Admin can edit/delete (PUT/PATCH/DELETE).
-    """
-
-    def has_permission(self, request, view):
-        """
-        View level permission for all request - GET, POST, PUT, DELETE
-        """
-        if request.user and request.user.is_authenticated:
-            return True
-
-        return False
-
-    def has_object_permission(self, request, view, obj):
-        """
-        Object-level permission for detail views (GET, PUT, DELETE)
-        Authenticated/logged in users can read the objects
-        To edit or delete the object, user must be the object owner or an admin
-        """
-
-        if request.method in SAFE_METHODS:
-            return True
-
-        if request.user.is_staff or obj.owner == request.user:
-            return True
-
-        return False
-
-
 class ExerciseListCreateView(generics.ListCreateAPIView):
     serializer_class = ExerciseSerializer
-    permission_classes = (IsOwnerAdminOrReadOnly,)
 
     def get_queryset(self):
         user = self.request.user
-
+        
         if user.is_staff:
             return Exercise.objects.all()
-
+            
         return Exercise.objects.filter(owner=user)
 
     def perform_create(self, serializer):
@@ -59,12 +24,11 @@ class ExerciseListCreateView(generics.ListCreateAPIView):
 
 class ExerciseDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ExerciseSerializer
-    permission_classes = (IsOwnerAdminOrReadOnly,)
 
     def get_queryset(self):
         user = self.request.user
-
+        
         if user.is_staff:
             return Exercise.objects.all()
-
+            
         return Exercise.objects.filter(owner=user)
