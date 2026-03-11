86bfc75253bde61ff61c964bd603c86f37a31856
Guram Mamporia <gurammamphoria@gmail.com>
2026-03-12 01:17:28 +0400

    revised workouts/views.py

--- workouts/views.py
+++ workouts/views.py
@@ -5,18 +5,16 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import WorkoutSession
 from .serializers import WorkoutSessionSerializer
-from .permissions import IsOwnerOrReadOnly
 
 
 class WorkoutSessionListCreateView(generics.ListCreateAPIView):
     serializer_class = WorkoutSessionSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return (
-            WorkoutSession.objects.prefetch_related("exercises__sets")
+            WorkoutSession.objects.filter(user=self.request.user)
+            .prefetch_related("exercises__sets")
             .select_related("user")
-            .all()
         )
 
     def perform_create(self, serializer):
@@ -25,37 +23,25 @@ class WorkoutSessionListCreateView(generics.ListCreateAPIView):
 
 class WorkoutSessionDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = WorkoutSessionSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
         return (
-            WorkoutSession.objects.prefetch_related("exercises__sets")
+            WorkoutSession.objects.filter(user=self.request.user)
+            .prefetch_related("exercises__sets")
             .select_related("user")
-            .all()
-        )
-
-
-class MySessionsView(generics.ListAPIView):
-    serializer_class = WorkoutSessionSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return WorkoutSession.objects.filter(user=self.request.user).prefetch_related(
-            "exercises__sets"
         )
 
 
 class SessionsByDateView(generics.ListAPIView):
     serializer_class = WorkoutSessionSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = WorkoutSession.objects.filter(
             user=self.request.user
-        ).prefetch_related("exercises__sets")
+        ).prefetch_related("exercises__sets").select_related("user")
 
         session_date = self.request.query_params.get("date")
         if session_date:
             queryset = queryset.filter(date=session_date)
-
+            
         return queryset
