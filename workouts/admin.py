5d2b2cf4d2441297821bb147bfc828722bbe0836
Guram Mamporia <gurammamphoria@gmail.com>
2026-03-12 21:42:25 +0400

    final commit

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import WorkoutSession
+from .models import WorkoutSession, WorkoutExercise, WorkoutSet
 
 admin.site.register(WorkoutSession)
+admin.site.register(WorkoutExercise)
+admin.site.register(WorkoutSet)
